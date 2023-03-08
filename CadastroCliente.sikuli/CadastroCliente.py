cb5f2500e05a57c044495a90e321c6329c3a2b0d
Thiago Ferraz <103955668+ferrazthiago13@users.noreply.github.com>
2023-03-07 23:17:49 -0300

    Cadastro de cliente

---
+++ CadastroCliente.sikuli/CadastroCliente.py
@@ -0,0 +1,33 @@
+click("1678235501059.png")
+click("1678235586363.png")
+click("1678235615021.png")
+type('Consumidor Final')
+click(Pattern("1678235707720.png").targetOffset(-56,0))
+type('82082266028')
+click("1678241001071.png")
+click("1678241070119.png")
+type("1678239955788.png", 'cliente@gmail.com')
+type(Pattern("1678239993223.png").targetOffset(-56,0), '021996566545')
+type("1678240041022.png", 'Rua teste')
+type("1678240100230.png", '123')
+type("1678240114967.png", 'Apto 305')
+type("1678240147234.png", 'Centro')
+type(Pattern("1678240163528.png").targetOffset(-59,0), '25954000')
+click("1678241264060.png")
+hover(Pattern("1678240646026.png").targetOffset(-1,68))
+click("1678241487291.png")
+click("1678240963756.png")
+click(Pattern("1678240317295.png").targetOffset(-82,-6))
+click(Pattern("1678241650629.png").targetOffset(13,7))
+find("1678241597356.png")
+
+
+
+
+
+
+
+
+
+
+
