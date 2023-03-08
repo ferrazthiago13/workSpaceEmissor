4a6e4af63af7baf0a5be83089a150b12e2c50cba
Thiago Ferraz <103955668+ferrazthiago13@users.noreply.github.com>
2023-03-08 00:44:10 -0300

    Cadastro Transportadora

---
+++ CadastroTransportadora.sikuli/CadastroTransportadora.py
@@ -0,0 +1,20 @@
+click("1678244837063.png")
+click("1678244849081.png")
+type("1678244989500.png", 'Frilog')
+click("1678245218253.png")
+click("1678245359817.png")
+type(Pattern("1678245384079.png").targetOffset(-57,5), '77533476000112')
+type("1678245456954.png", '248072641')
+type("1678245503174.png", 'Rua Duque de Caxias, 20')
+click("1678245556064.png")
+click("1678245939278.png")
+click("1678246038867.png")
+click("1678245993088.png")
+click("1678245653124.png")
+exists("1678246347154.png")
+click("1678246496100.png")
+
+
+
+
+
