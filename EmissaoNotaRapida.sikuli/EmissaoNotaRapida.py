3a6a7fdc51be3d34135eabde2cf7fe279f547a7c
Thiago Ferraz <103955668+ferrazthiago13@users.noreply.github.com>
2023-03-09 23:46:50 -0300

    Emissão Nfe rápida

---
+++ EmissaoNotaRapida.sikuli/EmissaoNotaRapida.py
@@ -0,0 +1,21 @@
+click("1678332528564.png")
+click("1678332549085.png")
+click(Pattern("1678333265517.png").targetOffset(166,4))
+doubleClick("1678414336298.png")
+click("1678332566564.png")
+click(Pattern("1678332812936.png").targetOffset(350,3))
+doubleClick("1678332606149.png")
+click("1678333046797.png")
+click("1678333068910.png")
+click(Pattern("1678333113358.png").targetOffset(160,7))
+doubleClick("1678333166603.png")
+click("1678333183411.png")
+click("1678333194940.png")
+click("1678333930541.png")
+wait("1678333651112.png", 18)
+click("1678334155507.png")
+click("1678415053152.png")
+
+
+
+ 
