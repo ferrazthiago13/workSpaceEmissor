f580f95c36d150570ea74bfb9296607925b7364b
Thiago Ferraz <103955668+ferrazthiago13@users.noreply.github.com>
2023-03-09 23:33:57 -0300

    Cancelamento de NFe

---
+++ CancelamentoNfe.sikuli/CancelamentoNfe.py
@@ -0,0 +1,10 @@
+click("1678414656203.png")
+click("1678414672753.png")
+wait("1678414742666.png")
+type("1678414763320.png", 'Cancelamento teste de NFe')
+click("1678414813986.png")
+wait("1678414847050.png", 15)
+click("1678414855772.png")
+
+
+
